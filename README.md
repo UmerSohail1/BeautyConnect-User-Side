fac1c031cc21e5cb35842f57cb3a68ad78616668
Warraich07 <talhazubair7712@gmail.com>
2025-05-22 13:11:53 +0500

    Latest Code

---
+++ README.md
@@ -0,0 +1,21 @@
+👤 User Side
+Key Features:
+
+🗓 Real-Time Booking: Users can view available time slots for barbers and book appointments accordingly
+
+🔔 Appointment Notifications: Receive confirmation and status updates for all appointments
+
+❌ Cancel Appointment with Reason: Users can cancel appointments and must provide a reason for cancellation
+
+📂 Booking History: View list of all active, completed, and canceled appointments
+
+💇 Barber Side
+Key Features:
+
+📥 Booking Acknowledgment: Receive instant notifications when a user books an appointment
+
+✅ Complete Appointment: Mark appointments as completed after the service
+
+❌ Cancel Appointment with Reason: Barbers can cancel appointments by stating a reason
+
+📂 Appointment History: Track all upcoming, completed, and canceled bookings
---
+++ lib/api_services/notifications_services.dart
@@ -0,0 +1,60 @@
+import 'dart:developer';
+import 'package:http/http.dart' as http;
+import 'package:googleapis_auth/auth_io.dart' as auth;
+class PushNotifications {
+  static Future<String> getAccessTokens() async {
+    final serviceAccountJson = {
+
+    };
+    List<String> scopes = [
+      'https://www.googleapis.com/auth/firebase.messaging',
+      'https://www.googleapis.com/auth/firebase.database',
+      'https://www.googleapis.com/auth/userinfo.email',
+    ];
+    http.Client client = http.Client();
+    auth.AccessCredentials credentials = await auth.obtainAccessCredentialsViaServiceAccount(
+      auth.ServiceAccountCredentials.fromJson(serviceAccountJson),
+      scopes,
+      client,
+    );
+    client.close();
+    return credentials.accessToken.data;
+  }
+
+  static sendNotificationToSelectedDriver(String deviceToken) async {
+    final String serverKeyAccessToken = await getAccessTokens();
+    log(serverKeyAccessToken);
+
+  }
+}
+
+
+// String endPointFirebaseCloudMessaging = 'https://fcm.googleapis.com/v1/projects/beauty-connect-9d187/messages:send';
+// final Map<String, dynamic> message = {
+//   'message': {
+//     'token': deviceToken,
+//     'notification': {
+//       'title': 'title is',
+//       'body': 'body is'
+//     },
+//     // 'data':
+//     // {
+//     //   'tripID': tripId
+//     // }
+//   }
+// };
+//
+// final http.Response response = await http.post(
+//   Uri.parse(endPointFirebaseCloudMessaging),
+//   headers: <String, String>{
+//     'Content-Type': 'application/json',
+//     'Authorization': 'Bearer $serverKeyAccessToken'
+//   },
+//   body: jsonEncode(message),
+// );
+//
+// if (response.statusCode == 200) {
+//   print("notification sent");
+// } else {
+//   print("notification not sent ${response.statusCode}");
+// }
